55cf60c51c8689736d446b4cbd4944892cf82b72
Reinout Eppinga <reinout@reinout.com>
2016-12-28 14:15:18 +0100

    refactor(PoiList): convert to stateless functional component

--- src/PoiList/PoiList.js
+++ src/PoiList/PoiList.js
@@ -1,35 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-class PoiList extends React.Component {
-
-    render() {
-        return (
-            <ul className="poi-list">
-                { this.props.items.map(item => this.renderItem(item))}
-            </ul>
-        )
-    }
-
-    renderItem(item) {
-        return (
-            <li className="poi-list-item">
-                <Link to={'/poi/' + item.id} className="poi-list-item-anchor">
-                    <h1 className="poi-list-item-title">{ item.title }</h1>
-                    <div className="poi-list-item-details">
-                        <img className="poi-list-item-img"
-                             src={ item.imgUrl }
-                             width="375" height="200"
-                             alt={ item.title }/>
-                        <div className="poi-list-item-labels">
-                            <span className="poi-list-item-price">€ { item.price },-</span>
-                            <span className="poi-list-item-skip">Skip the line</span>
+const PoiList = (props) => (
+    <ul className="poi-list">
+        { props.items.map(item => {
+            return (
+                <li className="poi-list-item">
+                    <Link to={'/poi/' + item.id} className="poi-list-item-anchor">
+                        <h1 className="poi-list-item-title">{ item.title }</h1>
+                        <div className="poi-list-item-details">
+                            <img className="poi-list-item-img"
+                                 src={ item.imgUrl }
+                                 width="375" height="200"
+                                 alt={ item.title }/>
+                            <div className="poi-list-item-labels">
+                                <span className="poi-list-item-price">€ { item.price },-</span>
+                                <span className="poi-list-item-skip">Skip the line</span>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            </li>
-        )
-    }
-}
+                    </Link>
+                </li>
+            )
+        })}
+    </ul>
+);
 
 export default PoiList;
