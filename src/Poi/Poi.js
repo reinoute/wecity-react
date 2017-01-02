4837dc781e46dabf8cd02376174fcac51abdcb76
Reinout Eppinga <reinout@reinout.com>
2017-01-02 10:51:03 +0100

    feat(Poi): initial carousel markup and styling

--- src/Poi/Poi.js
+++ src/Poi/Poi.js
@@ -8,13 +8,18 @@ class Poi extends React.Component {
     constructor() {
         super();
 
-        this.state = {poi: {
-            facilities: [],
-            labels: [],
-            images: [],
-            tags: [],
-            ticketInformation: [],
-        }};
+        this.nextImage = this.nextImage.bind(this);
+
+        this.state = {
+            imageIndex: 0,
+            poi: {
+                facilities: [],
+                labels: [],
+                location: { coordinates: [] },
+                images: [],
+                tags: [],
+                ticketInformation: [],
+            }};
     }
 
     componentDidMount() {
@@ -25,39 +30,79 @@ class Poi extends React.Component {
         })
     }
 
+    nextImage() {
+
+        
+        const count = this.state.poi.images.length + 1; // add main image to count
+
+        if (this.state.imageIndex <= count) {
+            const newIndex = this.state.imageIndex + 1;
+
+            this.setState({ imageIndex: newIndex });
+        } else {
+            this.setState({imageIndex: 0});
+        }
+
+        console.log('-RE- index', this.state.imageIndex);
+        console.log('-RE- count', count);
+
+    }
+
     render() {
         return (
             <div className="poi">
                 <div className="poi-header">
-                    <Link to={'/'} className="poi-anchor-back"><span className="icon-chevron-left"> Back</span></Link>
+                    <Link to={'/'} className="poi-anchor-back"><
+                        span className="icon-chevron-left"> Back</span>
+                    </Link>
                 </div>
-                <ul className="poi-gallery">
-                    { this.renderImage(this.state.poi.mainImgUrl) }
-                    { this.state.poi.images.map(this.renderImage) }
-                </ul>
-                <section className="poi-section">
-                    <h1 className="poi-title">{ this.state.poi.title }</h1>
-                    <div className="poi-opening-hours">
-                        <span className="icon-open-now poi-icon"></span>
-                        Open until 18:00
-                    </div>
-                    <div className="poi-tickets">
-                        <span className="icon-ticket poi-icon"></span>
-                        <ul className="poi-ticket-list">
-                            { this.state.poi.ticketInformation.map(ticket => (
-                                <li className="poi-ticket-item" key={ ticket.id }>
-                                    <span className="poi-ticket-label">{ ticket.label }: </span>
-                                    <span className="poi-ticket-price">{ formatPrice(ticket.price) }</span>
-                                </li>
-                            )
-                        )}
-                        </ul>
-                    </div>
-                    <button className="button-primary" type="button">Get Tickets</button>
-                </section>
-                <section className="poi-section">
-                    <h2 className="poi-subtitle">Location</h2>
-                    <p>{ this.state.poi.location }</p>
+                <div className="poi-carousel">
+                    <button className="poi-carousel-button-previous" type="button">
+                        <span className="icon-chevron-left">Previous</span>
+                    </button>
+                    <ul className="poi-carousel-list" style={ {marginLeft: this.state.imageIndex * -100 + "%"} }>
+                        { this.renderImage(this.state.poi.mainImgUrl) }
+                        { this.state.poi.images.map(this.renderImage) }
+                    </ul>
+                    <button className="poi-carousel-button-next" type="button" onClick={this.nextImage}>
+                        <span className="icon-chevron-right">Next</span>
+                    </button>
+                </div>
+                <section className="poi-body">
+                    <section className="poi-summary">
+                        <h1 className="poi-title">{ this.state.poi.title }</h1>
+                        <div className="poi-opening-hours">
+                            <span className="icon-open-now poi-icon"></span>
+                            Open until 18:00
+                        </div>
+                        <div className="poi-tickets">
+                            <span className="icon-ticket poi-icon"></span>
+                            <ul className="poi-ticket-list">
+                                { this.state.poi.ticketInformation.map(ticket => (
+                                        <li className="poi-ticket-item" key={ ticket.id }>
+                                            <span className="poi-ticket-label">{ ticket.label }: </span>
+                                            <span className="poi-ticket-price">{ formatPrice(ticket.price) }</span>
+                                        </li>
+                                    )
+                                )}
+                            </ul>
+                        </div>
+                        <button className="button-primary" type="button">Get Tickets</button>
+                    </section>
+                    <section className="poi-section">
+                        <h2 className="poi-subtitle">Location</h2>
+                        <address className="poi-address">
+                            { this.state.poi.location.address }<br/>
+                            { this.state.poi.location.zipcode } { this.state.poi.location.city }
+                        </address>
+                        <a href={ 'https://www.google.com/maps/search/@' + this.state.poi.location.coordinates[0] + ',' + this.state.poi.location.coordinates[1] }>Show on map</a>
+                    </section>
+                    <section className="poi-section">
+                        <h2 className="poi-subtitle">Description</h2>
+                        <div className="poi-description">
+                            { this.state.poi.summary }
+                        </div>
+                    </section>
                 </section>
             </div>
         )
@@ -65,8 +110,8 @@ class Poi extends React.Component {
 
     renderImage(url, index) {
         return (
-            <li className="poi-gallery-item" key={ index }>
-                <img className="poi-gallery-img" src={url} alt={""} />
+            <li className="poi-carousel-item" key={ index }>
+                <img className="poi-carousel-img" src={url} alt={""} />
             </li>
         )
     }
