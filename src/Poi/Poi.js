48b79a6ea624adb14e3448f13397e29723bcb00f
Reinout Eppinga <reinout@reinout.com>
2017-01-03 14:12:40 +0100

    feat(Poi): add skip the line text

--- src/Poi/Poi.js
+++ src/Poi/Poi.js
@@ -89,20 +89,31 @@ class Poi extends React.Component {
                             </ul>
                         </div>
                         <button className="button-primary" type="button">Get Tickets</button>
+                        { this.state.poi.skipTheLine &&
+                            <div className="poi-skiptheline">
+                                <span className="poi-skiptheline-title">Skip the line</span>
+                                <p>During checkout you can choose the time you want to enter the museum.
+                                    You can use a special lane to access the museum to avoid standing in line.</p>
+                            </div>
+                        }
                     </section>
                     <section className="poi-section">
-                        <h2 className="poi-subtitle">Location</h2>
-                        <address className="poi-address">
-                            { this.state.poi.location.address }<br/>
-                            { this.state.poi.location.zipcode } { this.state.poi.location.city }
-                        </address>
-                        <a href={ 'https://www.google.com/maps/search/@' + this.state.poi.location.coordinates[0] + ',' + this.state.poi.location.coordinates[1] }>Show on map</a>
-                    </section>
-                    <section className="poi-section">
-                        <h2 className="poi-subtitle">Description</h2>
-                        <div className="poi-description">
+                        <h2 className="poi-subtitle">About</h2>
+                        <p className="poi-description">
                             { this.state.poi.summary }
-                        </div>
+                        </p>
+                        <p className="poi-tags">
+                            <strong>Tags: </strong>
+                            <ul className="poi-tag-list">
+                                { this.state.poi.tags.map(tag => <li className="poi-tag-item">{ tag.value }</li>) }
+                            </ul>
+                        </p>
+                        <ul className="poi-about-list">
+                            <li className={"poi-about-item poi-type icon-" + this.state.poi.type}>{ this.state.poi.type }</li>
+                            <li className="poi-about-item icon-mapswitch">{ this.state.poi.location.address } <a
+                                href={ 'https://www.google.com/maps/place/' + this.state.poi.location.coordinates[1] + ',' + this.state.poi.location.coordinates[0] }>Show
+                                on map</a></li>
+                        </ul>
                     </section>
                 </section>
             </div>
