222a59f1416487902873f0018d2c8bf5e66c7e30
Reinout Eppinga <reinout@reinout.com>
2017-01-04 16:27:52 +0100

    refactor(api): update all data at once

--- src/PoiListContainer/PoiListContainer.js
+++ src/PoiListContainer/PoiListContainer.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import PoiList from '../PoiList/PoiList';
-import {getAllPois, getTopPois, updateData} from '../storage';
-import {fetchAllPois} from '../api';
+import {getTopPois} from '../storage';
 
 class PoiListContainer extends React.Component {
 
     constructor() {
         super();
-        // getAllPois().then(items => {
-        //     console.log('-RE- items', items);
-        // })
-
-        updateData().then(result => console.log('-RE- updated!', result))
 
         // set initial state
         this.state = {
--- src/storage.js
+++ src/storage.js
@@ -6,59 +6,46 @@ const ALL_POIS_KEY = "POIS";
 const LAST_UPDATED_KEY = "LAST_UPDATED";
 const ONE_DAY = 1000 * 60 * 60 * 24; // number of milliseconds in one day
 
-const getAllPois = () => {
-    return localforage.getItem(LAST_UPDATED_KEY).then(timestamp => {
-        // return local data if not older than 1 day
-        if (timestamp && (Date.now() - timestamp) < ONE_DAY) {
-            return localforage.getItem(ALL_POIS_KEY);
-        } else {
-            // otherwise fetch new data, store it locally and update timestamp
-            return fetchAllPois().then(items => {
-                return localforage.setItem(ALL_POIS_KEY, items).then((items) => {
-                    return localforage.setItem(LAST_UPDATED_KEY, Date.now()).then(() => {
-                        return items;
-                    });
-                })
-            })
-        }
-    })
-}
-
-const updateData = () => {
-    return Promise.all([
-                fetchAllPois(),
-                fetchTopPois()])
-                    .then(response => {
-                        Promise.all([
-                            localforage.setItem(ALL_POIS_KEY, response[0]),
-                            localforage.setItem(TOP_POIS_KEY, response[1])
-                        ])
-                        .then(data => {
-                            localforage.setItem(LAST_UPDATED_KEY, Date.now())
-                                .then(() => { return data })
-                        })
-                        .catch(error => console.log('Error updating local data: ', error))
-                    }
+const updateData = () =>
+    Promise.all([
+        fetchAllPois(),
+        fetchTopPois()])
+        .then(data =>
+            Promise.all([
+                localforage.setItem(ALL_POIS_KEY, data[0]),
+                localforage.setItem(TOP_POIS_KEY, data[1])
+            ])
+                .then(data =>
+                    localforage.setItem(LAST_UPDATED_KEY, Date.now())
+                        .then(() => data)
                 )
-}
+                .catch(error => console.log('Error updating local data: ', error)));
 
+const getAllPois = () => {
+    return Promise.all([
+        localforage.getItem(ALL_POIS_KEY),
+        localforage.getItem(LAST_UPDATED_KEY)
+    ]).then(values => {
+        const pois = values[0];
+        const lastUpdated = values[1];
 
-const getTopPois = () => {
-    return localforage.getItem(LAST_UPDATED_KEY).then(timestamp => {
-        // return local data if not older than 1 day
-        if (timestamp && (Date.now() - timestamp) < ONE_DAY) {
-            return localforage.getItem(TOP_POIS_KEY);
-        } else {
-            // otherwise fetch new data, store it locally and update timestamp
-            return fetchTopPois().then(items => {
-                return localforage.setItem(TOP_POIS_KEY, items).then((items) => {
-                    return localforage.setItem(LAST_UPDATED_KEY, Date.now()).then(() => {
-                        return items;
-                    });
-                })
-            })
-        }
+        if (pois && lastUpdated && (Date.now() - lastUpdated) < ONE_DAY)
+            return localforage.getItem(ALL_POIS_KEY);
+        else return updateData().then(data => data[0])
     })
 }
 
-export {getAllPois, getTopPois, updateData};
+const getTopPois = () =>
+    Promise.all([
+        localforage.getItem(TOP_POIS_KEY),
+        localforage.getItem(LAST_UPDATED_KEY)
+    ]).then(values => {
+        const pois = values[0];
+        const lastUpdated = values[1];
+
+        if (pois && lastUpdated && (Date.now() - lastUpdated) < ONE_DAY)
+            return localforage.getItem(TOP_POIS_KEY);
+        else return updateData().then(data => data[1])
+    })
+
+export {getAllPois, getTopPois};
