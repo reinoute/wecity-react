80e288cc69fa1b4f581b4caa08835100bfbdfd4b
Reinout Eppinga <reinout@reinout.com>
2017-01-05 13:51:01 +0100

    feat(api): store more properties for all pois

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://tst.wecity.amsterdam/wecity-3';
-const imgPostfix = '?variant=detail-image';
+const imgVariant = '?variant=detail-image';
 
 const fetchAllPois = () => {
     const query = '/poi/top?count=5000&mode=LOCAL&locale=en&travelMode=walking';
@@ -9,7 +9,29 @@ const fetchAllPois = () => {
             return response.json()
         })
         .then(function (data) {
-            return data;
+            return data.map(item => {
+                const images = item.media;
+                const mainImg = images ? images[0] : '';
+                const info = item.ticketInformation ? item.ticketInformation[0] : '';
+                const features = item.features;
+
+                return {
+                    id: item.id,
+                    title: item.title,
+                    imgUrl: (mainImg && mainImg.url) ? baseUrl + mainImg.url + imgVariant : '',
+                    images: images ? item.media.map(img => baseUrl + img.url + imgVariant) : '',
+                    price: (info && info.price) ? info.price : '',
+                    skipTheLine: (info && info.information) ? info.information.indexOf('avoid the standard waiting line') >= 0 : false,
+                    summary: item.summary,
+                    description: item.description,
+                    location: item.location,
+                    categories: (features && features.category) ? features.category : '',
+                    facilities: (features && features.facility) ? features.facility : '',
+                    labels: (features && features.label) ? features.label : '',
+                    tags: (features && features.tag) ? features.tag : '',
+                    ticketInformation: info
+                }
+            })
         })
         .catch(() => console.log(`Error fetching data from ${baseUrl}`));
 }
@@ -49,8 +71,8 @@ const getPoi = (id) => {
                 id: item.id,
                 type: item.type,
                 title: item.title,
-                mainImgUrl: baseUrl + item.media.filter(item => item.main)[0].url + imgPostfix,
-                images: item.media.map(img => baseUrl + img.url + imgPostfix),
+                mainImgUrl: baseUrl + item.media.filter(item => item.main)[0].url + imgVariant,
+                images: item.media.map(img => baseUrl + img.url + imgVariant),
                 summary: item.summary,
                 description: item.description,
                 location: item.location,
