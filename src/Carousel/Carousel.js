81a75bbb09c966c649ce63993c8148c1581d8614
Reinout Eppinga <reinout@reinout.com>
2017-01-06 11:30:46 +0100

    feat(Carousel): hide show next/previous buttons when not relevant

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Carousel = (props) => (
     <div className="carousel">
-        <button className="carousel-button-previous" type="button" onClick={props.previousImage}>
-            <span className="icon-chevron-left"></span>
-            <span className="carousel-button-text">Previous</span>
-        </button>
+        { props.index > 0 &&
+            <button className="carousel-button-previous" type="button" onClick={props.previousImage}>
+                <span className="icon-chevron-left"></span>
+                <span className="carousel-button-text">Previous</span>
+            </button>
+        }
         <ul className="carousel-list">
             { props.items.map((item, index) => {
                 return (
@@ -16,10 +18,12 @@ const Carousel = (props) => (
                 )
             })}
         </ul>
-        <button className="carousel-button-next" type="button" onClick={props.nextImage}>
-            <span className="icon-chevron-right"></span>
-            <span className="carousel-button-text">Next</span>
-        </button>
+        { props.index < props.items.length -1  &&
+            <button className="carousel-button-next" type="button" onClick={props.nextImage}>
+                <span className="icon-chevron-right"></span>
+                <span className="carousel-button-text">Next</span>
+            </button>
+        }
     </div>
 );
 
