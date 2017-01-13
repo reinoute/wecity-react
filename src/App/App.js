0753363d8726b05b919ba491f81ad253b5f3d12c
Reinout Eppinga <reinout@reinout.com>
2017-01-13 11:38:30 +0100

    refactor(App): move pois state to App

--- src/App/App.js
+++ src/App/App.js
@@ -3,15 +3,25 @@ import { Match } from 'react-router';
 import Navigation from '../Navigation/Navigation';
 import FiltersContainer from '../FiltersContainer/FiltersContainer';
 import PoiListContainer from '../PoiListContainer/PoiListContainer';
+import {getPois} from '../storage';
 
 class App extends React.Component {
+
+    state = { pois: [] };
+
+    componentDidMount() {
+        const onlyBookable = true;
+
+        getPois(onlyBookable)
+            .then(items => this.setState({pois: items}));
+    }
 
     render() {
         return (
             <div>
                 <Navigation path={this.props.pathname}/>
                 <Match pattern="/search" component={FiltersContainer}/>
-                <PoiListContainer/>
+                <PoiListContainer items={this.state.pois}/>
             </div>
         )
     }
--- src/PoiListContainer/PoiListContainer.js
+++ src/PoiListContainer/PoiListContainer.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import PoiList from '../PoiList/PoiList';
-import {getPois} from '../storage';
 
 class PoiListContainer extends React.Component {
 
-    constructor() {
-        super();
-
-        // set initial state
-        this.state = {
-            pois: []
-        };
-    }
-
-    componentDidMount() {
-        const onlyBookable = this.props.onlyBookable;
-
-        getPois(onlyBookable)
-            .then(items => this.setState({pois: items}));
-    }
-
     render() {
-        return ( <PoiList items={this.state.pois} /> )
+        return ( <PoiList items={this.props.items} /> )
     }
 }
 
