2878bb16fdfb36c6f3841328dd2d7888e95240f9
Reinout Eppinga <reinout@reinout.com>
2017-01-16 14:28:15 +0100

    feat(Navigation): align logo to the left

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -5,22 +5,26 @@ import logoText from './wecity-logo-text.svg';
 const Navigation = (props) => {
     return (
         <header className="navigation" role="banner">
-            <div className="navigation-logo">
-                <span className="icon-logo-wecity"></span>
-                <img className="navigation-logo-text" src={ logoText } alt="weCity"/>
+            <div className="navigation-container">
+                <div className="navigation-logo">
+                    <span className="icon-logo-wecity"></span>
+                    <img className="navigation-logo-text" src={ logoText } alt="weCity"/>
+                </div>
+                <nav role="navigation">
+                    <ul className="navigation-nav-list">
+                        <li className="navigation-nav-item">
+                            <Link to={'/'}
+                                  className={ "navigation-item-anchor " + (props.path === "/" ? "is-active" : "")}>Top
+                                10</Link>
+                        </li>
+                        <li className="navigation-nav-item">
+                            <Link to={'/search'}
+                                  className={ "navigation-item-anchor " + (props.path === "/search" ? "is-active" : "")}>All
+                                activities</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <nav role="navigation">
-                <ul className="navigation-nav-list">
-                    <li className="navigation-nav-item">
-                        <Link to={'/'}
-                              className={ "navigation-item-anchor " + (props.path === "/" ? "is-active" : "")}>Top 10</Link>
-                    </li>
-                    <li className="navigation-nav-item">
-                        <Link to={'/search'}
-                              className={ "navigation-item-anchor " + (props.path === "/search" ? "is-active" : "")}>All activities</Link>
-                    </li>
-                </ul>
-            </nav>
         </header>
     )
 }
