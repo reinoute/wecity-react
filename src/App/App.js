d768ba7641f774df3035efef8d8a5a8fb59e7f54
Reinout Eppinga <reinout@reinout.com>
2017-01-20 16:14:29 +0100

    feat(App): show filters component within search context

--- src/App/App.js
+++ src/App/App.js
@@ -6,15 +6,24 @@ import FiltersContainer from '../FiltersContainer/FiltersContainer';
 class App extends React.Component {
 
     state = {
+        context: 'home',
         items: [],
         filteredItems: [],
         activeFilterKeys: []
     }
 
-    componentDidMount = () =>
+    componentDidMount = () => {
+        const context = this.props.pathname.includes('top10') ? 'home' : 'search';
+
         getActivities()
             .then(items =>
-                this.setState({items, filteredItems: this.filterItems(items, this.state.activeFilterKeys)}));
+                this.setState({
+                    items,
+                    filteredItems: this.filterItems(items, []),
+                    context
+                }));
+    }
+
 
     addActiveFilterKey = (key) => {
         const keys = this.state.activeFilterKeys.slice(0); // clone keys
@@ -36,13 +45,13 @@ class App extends React.Component {
         })
     }
 
-    filterItems = (items, keys) => {
+    filterItems = (items, filterKeys) => {
         const onlyBookableActivities = this.props.pathname.includes('top10')
         const filteredItems = items
             // in top10 context, filter on items that are bookable
             .filter(item => onlyBookableActivities ? item.isBookable : true)
             // filter on item type
-            .filter(item => keys.length > 0 ? keys.indexOf(item.type) >= 0 : true)
+            .filter(item => filterKeys.length > 0 ? filterKeys.indexOf(item.type) >= 0 : true)
 
         // when no items match the filter criteria, return all items
         return filteredItems.length > 0 ? filteredItems : items;
